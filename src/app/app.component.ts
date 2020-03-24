766714f0265eb8c6b81670db0264859fac96814a
Jessica Hempel <jessica.hempel@avency.de>
2020-03-24 15:29:06 +0100

    feat: create user fromgroup to get and store user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-step-wizard';
+export class AppComponent implements OnInit {
+  userForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {  }
+  ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      gender: new FormControl(null, Validators.required),
+      first: new FormControl(null, Validators.required),
+      last: new FormControl(null, Validators.required),
+      phone: new FormControl(null),
+      email: new FormControl(null, Validators.required),
+      dateOfBirth: new FormControl(null, Validators.required),
+      address: new FormGroup({
+        street: new FormControl(null),
+        number: new FormControl(null),
+        zipCode: new FormControl(null),
+        city: new FormControl(null),
+        country: new FormControl(null),
+      }),
+    });
+  }
 }
