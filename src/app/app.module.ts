fa580b9bbf268fa92d764cd0a2846c2e4e602510
Jessica Hempel <jessica.hempel@avency.de>
2020-03-24 11:47:50 +0100

    feat: init user info component as second step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AccountInfosComponent } from './account-infos/account-infos.component';
+import { UserInfosComponent } from './user-infos/user-infos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccountInfosComponent,
+    UserInfosComponent,
   ],
   imports: [
     BrowserModule
