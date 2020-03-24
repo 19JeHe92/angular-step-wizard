6445ad427d94a70927097200d3731e24f7c9a1b9
Jessica Hempel <jessica.hempel@avency.de>
2020-03-24 11:48:04 +0100

    feat: init address info component as third step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AccountInfosComponent } from './account-infos/account-infos.component';
 import { UserInfosComponent } from './user-infos/user-infos.component';
+import { AddressInfosComponent } from './address-infos/address-infos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccountInfosComponent,
     UserInfosComponent,
+    AddressInfosComponent
   ],
   imports: [
     BrowserModule
