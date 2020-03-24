954dc00f3963345581212ad31ec8741c95789702
Jessica Hempel <jessica.hempel@avency.de>
2020-03-24 15:29:38 +0100

    feat: create basic step wizard structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { faCheck} from '@fortawesome/free-solid-svg-icons';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +10,55 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  @ViewChild('staticTabs', {static: true}) staticTabs: TabsetComponent;
   userForm: FormGroup;
+  faCheck = faCheck;
+  finished = false;
+  success = false;
+  error = false;
+  onSend = false;
+
   constructor(
     private formBuilder: FormBuilder,
+    private dataService: DataService,
   ) {  }
+
+  selectTab(tabId: number) {
+    this.staticTabs.tabs[tabId].active = true;
+    if (tabId > 0) {
+      this.staticTabs.tabs[tabId - 1].customClass = 'wasActive';
+    }
+  }
+
+  onSelectTab() {
+    this.finished = false;
+    this.staticTabs.tabs[2].disabled = true;
+  }
+
+  onDeSelect(data: TabDirective): void {
+    data.customClass = 'wasActive';
+  }
+
+  onFinish(tabId: number) {
+    this.finished = true;
+    this.staticTabs.tabs[tabId].disabled = false;
+    this.selectTab(tabId);
+  }
+
+  onSubmit() {
+    this.onSend = true;
+    this.dataService.sendData(this.userForm.value)
+      .subscribe(
+        (response) => {
+          this.success = true;
+          this.error = false;
+        },
+        (error) => {
+          this.success = false;
+          this.error = true;
+        });
+  }
+
   ngOnInit() {
     this.userForm = this.formBuilder.group({
       gender: new FormControl(null, Validators.required),
