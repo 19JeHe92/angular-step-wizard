3ff2ed50cf9a04b3b2154d33d1484dc17f44d77d
Jessica Hempel <jessica.hempel@avency.de>
2020-03-25 08:17:42 +0100

    feat: update data service to send data to mock api

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,7 +3,6 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export interface User {
-  userID: string;
   username: string;
   gender: string;
   title: string;
@@ -29,23 +28,6 @@ export class DataService {
   constructor(private httpClient: HttpClient) { }
 
   sendData(data): Observable<User> {
-    const testData = {
-      id: 3,
-      userID: 'USER_003',
-      gender: 'female',
-      first: 'Tina',
-      last: 'Tustermann',
-      phone: '09876543210',
-      email: 'tina.tustermann@mail.com',
-      dateOfBirth: '01.01.1970',
-      address: {
-        street: 'Mustergasse',
-        number: '99',
-        zipCode: '98765',
-        city: 'Musterstadt',
-        country: 'Deutschland'
-      }
-    };
-    return this.httpClient.post<User>('api/users', testData);
+    return this.httpClient.post<User>('api/users', data);
   }
 }
