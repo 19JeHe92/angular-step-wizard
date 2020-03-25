c1c1f0fc7afff45367adcf8cd9246c72f5dda693
Jessica Hempel <jessica.hempel@avency.de>
2020-03-25 08:12:34 +0100

    feat: update user-info component

--- src/app/user-infos/user-infos.component.ts
+++ src/app/user-infos/user-infos.component.ts
@@ -43,7 +43,7 @@ export class UserInfosComponent implements OnInit {
   countries = [
     {
       name: 'Deutschland',
-      value: 'germany'
+      value: 'DE'
     },
   ];
   constructor() { }
